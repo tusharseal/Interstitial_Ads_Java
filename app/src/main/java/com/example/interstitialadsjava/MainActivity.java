a576111278a323f68cbdd5340ec7490b267164c4
Tushar Seal <tusharseal1@gmail.com>
2023-05-16 11:49:49 +0530

    Basic Implementation of Interstitial Ads

---
+++ app/src/main/java/com/example/interstitialadsjava/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.interstitialadsjava;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
+public class MainActivity extends AppCompatActivity {
+    AdView adView;
+    InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adView = findViewById(R.id.adView);
+
+        //Step 1
+
+        MobileAds.initialize(this);
+
+        //Step 2
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        //Step 3
+        adView.loadAd(adRequest);
+
+        //For Interstitial_Ads Steps are follows.
+
+        //Step 1
+        InterstitialAd.load(this, getString(R.string.inter_ad_unit_id), adRequest, new InterstitialAdLoadCallback() {
+            @Override
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                super.onAdLoaded(interstitialAd);
+
+                //Step 2
+                mInterstitialAd = interstitialAd;
+
+                //Step 3
+                mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                    @Override
+                    public void onAdDismissedFullScreenContent() {
+                        super.onAdDismissedFullScreenContent();
+                    }
+
+                    @Override
+                    public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                        super.onAdFailedToShowFullScreenContent(adError);
+                    }
+
+                    @Override
+                    public void onAdImpression() {
+                        super.onAdImpression();
+                    }
+
+                    @Override
+                    public void onAdShowedFullScreenContent() {
+                        super.onAdShowedFullScreenContent();
+                        mInterstitialAd = null;
+                    }
+                });
+            }
+
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                super.onAdFailedToLoad(loadAdError);
+                Log.d("Error", loadAdError.toString());
+            }
+        });
+
+        //Step 4
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (mInterstitialAd != null)
+                    mInterstitialAd.show(MainActivity.this);
+                else
+                    Log.e("AdPending", "Ad is not Ready");
+            }
+        }, 5000);
+    }
+}
